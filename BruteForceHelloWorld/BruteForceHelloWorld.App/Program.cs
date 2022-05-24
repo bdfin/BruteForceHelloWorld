ac55c746062823c431fbef76999b02446e51c84c
Beau Findlay <beau.d.findlay@gmail.com>
2022-05-24 14:38:03 +0100

    Code commit.

---
+++ BruteForceHelloWorld/BruteForceHelloWorld.App/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+const string allPossibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz! ";
+
+string possibleCharacters = allPossibleCharacters;
+string soloution = "Hello World!";
+string answer = "";
+int currentLetterIndex = 0;
+
+var random = new Random();
+var stopWatch = new Stopwatch();
+
+stopWatch.Start();
+
+while (answer != soloution)
+{
+    char currentLetter = possibleCharacters[random.Next(possibleCharacters.Length)];
+
+    if (currentLetter != soloution[currentLetterIndex])
+    {
+        possibleCharacters.Replace($"{currentLetter}", "");
+
+        Console.WriteLine(answer + currentLetter);
+    }
+    else
+    {
+        var upperCaseVariant = char.ToUpperInvariant(soloution[currentLetterIndex]);
+
+        if (currentLetter == upperCaseVariant)
+        {
+            currentLetter = upperCaseVariant;
+        }
+
+        possibleCharacters = allPossibleCharacters;
+        answer += currentLetter;
+
+        Console.WriteLine(Environment.NewLine);
+        Console.WriteLine(answer);
+        currentLetterIndex++;       
+    }
+
+    Thread.Sleep(5);
+}
+
+stopWatch.Stop();
+
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine($"Time taken: {FormatStopWatchTime(stopWatch.Elapsed)}");
+
+static string FormatStopWatchTime(TimeSpan elapsedTime)
+{
+    return elapsedTime.ToString(@"mm\:ss\.fff");
+}
